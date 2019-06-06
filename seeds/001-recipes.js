ae35b3300c6ebb56c00e942433a8954bd1a0c206
joerlop <jj.erlich155@gmail.com>
2019-06-06 16:40:37 -0500

    Endpoints and data access file working as expected

--- seeds/001-recipes.js
+++ seeds/001-recipes.js
@@ -5,10 +5,10 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('recipes').insert([
-        {name: "Al Pastor", instructions: "Con mucho cuidado", dish_id: 1 },
-        {name: 'Al horno', instructions: "Muy despacio, 2", dish_id: 2 },
-        {name: 'Al horno', instructions: "Muy despacio, 3", dish_id: 3 },
-        {name: 'Al carbon', instructions: "Con mucho carbon, 2", dish_id: 2 }
+        {name: "Al Pastor", instructions: "Con mucho cuidado", dish_id: 11 },
+        {name: 'Al horno', instructions: "Muy despacio, 2", dish_id: 12 },
+        {name: 'Al horno', instructions: "Muy despacio, 3", dish_id: 13 },
+        {name: 'Al carbon', instructions: "Con mucho carbon, 2", dish_id: 12 }
       ]);
     });
 };
